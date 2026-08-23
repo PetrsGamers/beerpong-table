aac2d6b6a9fffeb29c16ce9ee9555e4eae7cbc82
Tadeas Kozub <ted@macbookm4.com>
2026-08-23 23:04:59 +0200

    fix: add src/proxy.ts generated by Next.js codemod

---
+++ src/proxy.ts
@@ -0,0 +1,23 @@
+import { NextRequest, NextResponse } from "next/server";
+import { getSession } from "./actions/auth";
+
+export default async function proxy(req: NextRequest) {
+  // 2. Check if the current route is protected
+  const path = req.nextUrl.pathname;
+  const currentPageIsLogin = path === "/login";
+  const isPublicPage = path.startsWith("/public");
+
+  // 3. Decrypt the session from the cookie
+  const session = await getSession();
+
+  // 5. Redirect to /login if the user is not authenticated (skip for public pages)
+  if (!currentPageIsLogin && !isPublicPage && !session?.userId) {
+    return NextResponse.redirect(new URL("/login", req.nextUrl));
+  }
+  return NextResponse.next();
+}
+
+// Routes Middleware should not run on
+export const config = {
+  matcher: ["/((?!api|_next/static|_next/image|.*\\.png$).*)"],
+};
